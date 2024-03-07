5473e6b898f4a80a1a432b3beec4c8a2d6db950b
adithya-naik <jatothadithyanaik@gmail.com>
2024-03-07 23:29:31 +0530

    added new file

--- first.py
+++ first.py
@@ -1,3 +1,9 @@
+# its like four step verification process
+# 1. first we need to import
+# 2. next create a object of Tk()
+# 3. pack all the other objects
+# 4. run the main loop
+
 from tkinter import *
 
 root = Tk()
---
+++ layouts.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+
+frame1 = Frame(root)
+frame1.pack()
+frame2 = Frame(root)
+frame2.pack()
+root.mainloop()
