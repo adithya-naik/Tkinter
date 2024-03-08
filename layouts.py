4f785c01db765d96c3d081915c0a8a9bdf6777ec
adithya-naik <jatothadithyanaik@gmail.com>
2024-03-08 11:44:40 +0530

    Update layouts.py

--- layouts.py
+++ layouts.py
@@ -2,8 +2,14 @@ from tkinter import *
 
 root = Tk()
 
-frame1 = Frame(root)
-frame1.pack()
-frame2 = Frame(root)
-frame2.pack()
+topframe = Frame(root)
+topframe.pack()
+bottomframe = Frame(root)
+bottomframe.pack(side= BOTTOM)
+but1 = Button(root,text="YES",fg="red",bg="yellow")
+but1.pack()
+but2 = Button(root,text="YES",fg="red",bg="yellow")
+but2.pack(fill=X)
+but3 = Button(root,text="YES",fg="red",bg="yellow")
+but3.pack(fill=X)
 root.mainloop()
